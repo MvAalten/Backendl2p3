504ab1d49ca4dbf5ec3f08ddb2b8b09ea0a0b59b
MvAalten <157478216+MvAalten@users.noreply.github.com>
2025-04-15 11:27:51 +0200

    Post works i can create posts i haven't printed them yet thats the next step

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
@@ -16,6 +17,7 @@ class PostController extends Controller
         $incomingFields['body'] = strip_tags($incomingFields['body']);
         $incomingFields['user_id'] = auth()->id();
         Post::create($incomingFields);
+        return redirect('/');
     }
 
 }
